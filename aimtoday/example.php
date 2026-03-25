8bbd6f764b13f45e794764c26b3dc78efaf16724
tunnull-mc <mattfuller890@gmail.com>
2026-03-25 02:50:43 -0400

    Update example.php

--- aimtoday/example.php
+++ aimtoday/example.php
@@ -1,16 +1,1 @@
-<?php
 
-//Recreation of INI-ish format for Headlines in News Ticker within AIM.
-require_once("../autoloader.php");
-require_once("../library/SimplePie.php");
-$feed = new SimplePie();
-$feedurl = "https://news.ycombinator.com/rss";
-$feed->set_feed_url($feedurl);
-$feed->init();
-$feed->handle_content_type();
-echo("Title=MyNews\nSubSection=Top News\n");
-foreach($feed->get_items() as $item)
-    {
-        echo("Headline=".$item->get_title()."\nURL=".$item->get_permalink()."\n");
-    }
-?>
